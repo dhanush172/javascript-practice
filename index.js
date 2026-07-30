f5c45532360c28422a7b5509d12861120dedf121
dhanush172 <dhanushkottu25@gmail.com>
2026-07-30 17:59:04 +0530

    add java script changes
    Added functionality to group anagrams from a list of strings and find the minimum number in an array.

--- index.js
+++ index.js
@@ -7,3 +7,43 @@ for (i = 0; i < nums.length; i++) {
       console.log(true);
     }
 }
+
+let strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+let result = [];
+
+for (let i = 0; i < strs.length; i++) {
+  let found = false;
+
+  for (let j = 0; j < result.length; j++) {
+    let word1 = strs[i].split("").sort().join("");
+    let word2 = result[j][0].split("").sort().join("");
+
+    if (word1 === word2) {
+      result[j].push(strs[i]);
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    result.push([strs[i]]);
+  }
+}
+
+console.log(result);
+
+function findMin(nums) {
+  let min = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] < min) {
+      min = nums[i];
+    }
+  }
+
+  return min;
+}
+
+console.log(findMin([3, 4, 5, 6, 1, 2]));
+
+
